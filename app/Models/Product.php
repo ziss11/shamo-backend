5d6b29ffa8377098ed7ce10083710959734cf0cf
Abdul Azis <abdul.azizz1108@gmail.com>
2024-06-16 01:05:45 +0700

    Product model created

---
+++ app/Models/Product.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Hashing\HashManager;
+
+class Product extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'description',
+        'tags',
+        'category_id',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+
+    public function galleries(): HasMany
+    {
+        return $this->hasMany(ProductGallery::class, 'product_id', 'id');
+    }
+}
--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductCategory extends Model
@@ -11,4 +12,9 @@ class ProductCategory extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = ['name'];
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class, 'category_id', 'id');
+    }
 }

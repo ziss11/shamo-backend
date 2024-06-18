c55ac6f36a3e89a9cb60ca4ac74f6221743b84d7
Abdul Azis <abdul.azizz1108@gmail.com>
2024-06-18 18:22:41 +0700

    filament installed

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::get('/', function () {
+    return redirect('/admin');
+});
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
